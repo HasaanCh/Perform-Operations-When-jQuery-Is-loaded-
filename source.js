3b22370119aba5d5e9fe66eaec2e4f921f250587
Hasaan Jamil <hasaan.ch.13@gmail.com>
2024-10-22 20:17:45 +0500

    Simplified Code

--- source.js
+++ source.js
@@ -1,11 +1,8 @@
 (async () => {
     console.log("waiting for jQuery");
 
-    // It's an example
-    var menu_icon = document.querySelector('.qodef-mobile-header-opener');
-    menu_icon.addEventListener("click", function () {
-        menu_icon.classList.add("menu_opened");
-    });
+  
+    
 
 
     // Promise Resolving
@@ -15,20 +12,5 @@
 
     // Jquery Loaded All Pending operations under
     console.log("jQuery is loaded.");
-    setTimeout(function () {
-        jQuery(document).ready(function ($) {
-            if ($('.qodef-mobile-header-opener').hasClass('menu_opened')) {
-                var nani = setInterval(function () {
-
-                    if ($('.qodef--opened').length < 1) {
-                        $('.qodef-mobile-header-opener').click();
-                    } else {
-                        clearInterval(nani);
-                    }
-                }, 50);
-
-            }
-        });
-    }, 200);
 
 })();
